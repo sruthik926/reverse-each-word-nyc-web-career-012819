4d59229f9ae4f42a86761d189635ec592b164391
Sruthi Krishna <sruthik926@gmail.com>
2019-01-18 00:23:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(str)
+  newArr = []
   strArr = str.split
   strArr.collect do |x|
     x.reverse
   end
-    x.reverse.join("")
+   newArr >> x.reverse
+   newArr.join("")
 end
